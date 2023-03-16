46a1430d85b0b274aa7a2a0d13ddfdcdeead16b7
olejech <olejech@gmail.com>
2023-03-16 22:59:13 +0300

    feat(plugins): add ChatGPT

---
+++ plugins/chatGPT.lua
@@ -0,0 +1,14 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  lazy = false,
+  opts = {
+    keymaps = {
+      submit = "<C-b>",
+    },
+  },
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+}
